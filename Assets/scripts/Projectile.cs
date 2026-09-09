2d3b51bd1e367a6e8f98213518b19c0fa9c6bb91
Raphael-UCCELLI <uccelliraphael@gmail.com>
2026-09-09 19:42:58 +0200

    Add projectile forward movement

---
+++ Assets/scripts/Projectile.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
