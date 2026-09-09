4488c45a6e91dfb6dcb2bc70da423a5613a03e76
Raphael-UCCELLI <uccelliraphael@gmail.com>
2026-09-09 21:00:16 +0200

    Add SpellData ScriptableObject and Fireball asset

---
+++ Assets/scripts/SpellData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSpell", menuName = "SpellWorkshop/Spell")]
+public class SpellData : ScriptableObject
+{
+    public string spellName = "New Spell";
+    public int damage = 10;
+    public float projectileSpeed = 10f;
+}
