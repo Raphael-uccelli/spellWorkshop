6fc5c05515ff4fcf5bc37971fcd5cf6eeb186e8a
Raphael-UCCELLI <uccelliraphael@gmail.com>
2026-09-11 20:03:29 +0200

    Add Element enum to spells

---
+++ Assets/scripts/Element.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum Element
+{
+    None,
+    Fire,
+    Water,
+    Earth,
+    Lightning,
+    Light
+}
--- Assets/scripts/SpellData.cs
+++ Assets/scripts/SpellData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class SpellData : ScriptableObject
 {
     public string spellName = "New Spell";
+    public Element element = Element.None;
     public int damage = 10;
     public float projectileSpeed = 10f;
     public bool isPiercing = false;
