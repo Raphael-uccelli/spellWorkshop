17ff8f479fc45b9dae0d8f9b8fa67ab059365717
Raphael-UCCELLI <uccelliraphael@gmail.com>
2026-09-14 22:42:47 +0200

    Add health bar UI above enemies

--- Assets/scripts/Health.cs
+++ Assets/scripts/Health.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
+    [SerializeField] private Slider healthBar;
+
     private int currentHealth;
     private bool isDead = false;
 
@@ -12,6 +15,7 @@ public class Health : MonoBehaviour
     void Awake()
     {
         currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
     public void TakeDamage(int amount)
@@ -20,10 +24,19 @@ public class Health : MonoBehaviour
 
         currentHealth -= amount;
         Debug.Log(gameObject.name + " took " + amount + " damage. Health: " + currentHealth);
+        UpdateHealthBar();
 
         if (currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = (float)currentHealth / maxHealth;
         }
     }
 
